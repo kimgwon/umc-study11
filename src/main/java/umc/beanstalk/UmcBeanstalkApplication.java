29518e487954ec5e73fa76efbb69bd503c127db8
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-12-28 11:51:22 +0900

    :tada: Set up

---
+++ src/main/java/umc/beanstalk/UmcBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package umc.beanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcBeanstalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcBeanstalkApplication.class, args);
+    }
+
+}
